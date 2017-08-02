44a5f0dd39f7392472d0fb212e54eb4492d2d932
Monica Li <monica.yc.li@gmail.com>
2017-08-02 18:01:07 -0400

    add sublime settings

--- setup.sh
+++ setup.sh
@@ -10,6 +10,10 @@ for filename in settings/*; do
 	  ln -fs "$DIR/$filename" "$HOME/$(basename "$filename")"
 done
 
+# copy sublime settings
+for filename in sublime/*; do
+	cp -i "filename" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/$(basename "$filename")"
+
 # turn off detect hidden files
 shopt -u dotglob
 
