2f3296a930b1911e8ec774dcf99e58b8bae2aea4
Monica Li <monica.yc.li@gmail.com>
2018-11-11 11:55:01 -0500

    remove sublime settings

--- setup.sh
+++ setup.sh
@@ -10,11 +10,6 @@ for filename in settings/*; do
 	  ln -fs "$DIR/$filename" "$HOME/$(basename "$filename")"
 done
 
-# copy sublime settings
-for filename in sublime/*; do
-  cp -i "$filename" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/$(basename "$filename")"
-done
-
 # Specify the preferences directory for iTerm
 defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$DIR/iTerm"
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
